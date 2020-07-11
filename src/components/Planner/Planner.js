28a610f947f2758caa3b48fadd651b0b4a22d612
asketonim <anton.dubik33@gmail.com>
2020-07-11 15:56:55 +0300

    Perform cleaning and set up 2 planners

---
+++ src/components/Planner/Planner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Planner.css';
+
+const Planner = () => {
+  return (
+    <div className='Planner'>
+      
+    </div>
+  );
+};
+
+export default Planner;
---
+++ src/components/PlannerP5/PlannerP5.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import P5Wrapper from 'react-p5-wrapper';
+import { sketch } from './sketch';
+
+const PlannerP5 = () => {
+  return (
+    <div>
+      <P5Wrapper sketch={sketch}></P5Wrapper>
+    </div>
+  );
+}
+
+export default PlannerP5;
